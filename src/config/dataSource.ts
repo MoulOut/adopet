ce9dafa020479c75f9386134346c108b73be6d02
MoulOut <mmoulinlima@outlook.com>
2024-02-27 03:53:15 -0300

    feat: :sparkles: add: pet entity

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import PetEntity from '../entities/petEntity.entity';
 
 export const appDataSource = new DataSource({
   type: 'sqlite',
   database: './src/config/db.sqlite',
-  entities: [],
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: true,
 });
